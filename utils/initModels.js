f6b09df8aca21d40220f5c049ba03ae0acdcba54
Davsua <davidsuarezcarvajal98@gmail.com>
2022-03-17 18:50:41 -0500

    initModel update

--- utils/initModels.js
+++ utils/initModels.js
@@ -1,6 +1,17 @@
-const { Actor } = require('../models/actors.model');
-const { User } = require('../models/users.model');
-const { Movie } = require('../models/movies.model');
-const { Review } = require('../models/reviews.model');
+const { Actor } = require('../models/actor.model');
+const { User } = require('../models/user.model');
+const { Movie } = require('../models/movie.model');
+const { Review } = require('../models/review.model');
+const { ActorInMovie } = require('../models/actorInMovie.model');
 
-const initModels = () => {};
+const initModels = () => {
+  User.hasMany(Review);
+  Review.belongsTo(User);
+
+  Movie.hasMany(Review);
+  Review.belongsTo(Movie);
+
+  //relation many to many with intermediate ActorInMovie
+  Movie.belongsToMany(Actor, { through: ActorInMovie });
+  Actor.belongsToMany(Movie, { through: ActorInMovie });
+};
